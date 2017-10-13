a18b746a96a45cfae3476a3324abc5435bcdb02a
Estudiantes <Estudiantes@labing50410.udistrital.edu.co>
2017-10-13 16:50:59 -0500

    faltan conexiones entre la interfaz y la clase abstracta

---
+++ Conversor/src/conversor/Conversorconcreto.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversor;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public  class Conversorconcreto extends AConversor {
+
+    @Override
+    double grado2radian() {
+        double pi=3.141592654;
+        
+        return (dato*pi)/180;
+        
+    }
+
+    @Override
+    double farenheit2kelvin() {
+        
+
+  
+
+
+    }
+}
